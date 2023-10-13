cf9d9e6d4b10d151d1dc2f998c2cd59d08710048
grapple solutions <info@grapple-solutions.com>
2023-10-13 07:10:16 +0000

    first working draft with extended/generated spec for resources from lb4

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 rm -R target 2>/dev/null
 mkdir -p target 2>/dev/null
 
+# update the CRDS according to the latest loopback image
+./createCRDS.sh
+
 # build the crossplane package
 echo "build the package"
 kubectl crossplane build configuration --name=${PACKAGE} --ignore=".gitpod.yml,examples/*,hack/*,.github/*/*,target/*,*.sh,cluster/*,cluster/*/*,network/*,test/*,test/*/*,grsf/*"
--- createCRDS.sh
+++ createCRDS.sh
@@ -4,14 +4,43 @@ if ! yq >/dev/null 2>&1; then
         sudo chmod +x /usr/bin/yq
 fi
 
+echo "----"
+echo "extend CRD for DataSources"
+
+dstypes=("mysql" "postgres" "model" "repository" "rest-crud" "service" "openapi" "relation") 
+
+
+
+echo
+echo
+
+exit
 
 echo "----"
-echo "create CRD for discovery"
+BASELOCATION=".spec.versions[0].schema.openAPIV3Schema.properties.spec.properties"
 
-yq -i "del(.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.discoveries.items.properties.spec.properties.*)" grapi/definition.yaml
-
-for i in $(docker run --rm -it patrickriegler/loopback:v0.92 grep "this.option(" /usr/local/lib/node_modules/\@loopback/cli/generators/discover/index.js | sed "s,this.option(',," | sed "s,'\, {,,g"); do 
-    echo "create spec: $i"
-
+# for z in $(echo "discover controller model repository rest-crud service openapi relation"); do 
+clis=("discover" "controller" "model" "repository" "rest-crud" "service" "openapi" "relation") 
+crds=("discoveries" "controllers" "models" "repositories" "restcruds" "services" "openapis" "relations")
+c=0
+for z in ${clis[*]}; do 
+  echo "---"
+  cli=$z
+  crd=${crds[${c}]}
+  echo "extend CRD for $crd"
+  echo "cli: $cli"
+  echo "crd: $crd"
+  docker run --rm -it patrickriegler/loopback:v0.93 cat /usr/local/lib/node_modules/\@loopback/cli/generators/${cli}/index.js > ${cli}.js
+  yq -i "del(${BASELOCATION}.${crd}.items.properties.spec.properties.*)" grapi/definition.yaml
+  for i in $(grep "this.option(" ${cli}.js | sed "s,this.option(',," | sed "s,'\, {.*,,g" | tr -d '\r'); do
+    name=$i
+    # echo grep "this.option(.*${name}" -A6 ${cli}.js 
+    type=$(grep "this.option(.*${name}" -A6 ${cli}.js | grep -m1 "type:" | sed "s,^.*type: ,,g" | sed "s|,.*||g" | sed "s|}.*||g" | tr '[:upper:]' '[:lower:]')
+    echo "Patching: $name --- $type"
+    yq -i "${BASELOCATION}.${crd}.items.properties.spec.properties += {\"${name}\": { \"description\": \"${name}\", \"type\": \"${type}\" } }" grapi/definition.yaml
+  done
+  rm ${z}.js
+  ((c++))
 done
+
 
