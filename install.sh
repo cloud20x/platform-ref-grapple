f05a17d9d6faf18cb315705733026a8ef6991f8e
Patrick Riegler <patrick.riegler@kreuzwerker.ch>
2023-11-10 14:41:40 +0100

    improve install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@ else
 
     echo -n "${AWSID}" > ./access-key
     echo -n "${AWSKEY}" > ./secret-access-key
+    kubectl -n kube-system delete secret awssm-secret
     kubectl -n kube-system create secret generic awssm-secret --from-file=./access-key  --from-file=./secret-access-key 2>/dev/null || true
+    kubectl -n grpl-system delete secret awssm-secret || true
+    kubectl -n grpl-system create secret generic awssm-secret --from-file=./access-key  --from-file=./secret-access-key 2>/dev/null || true
 
 fi
 
