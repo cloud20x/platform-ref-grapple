517cd455e05e06eb7fb4128e7cf82030f8de0392
grapple-solutions <info@grapple-solutions.com>
2024-01-19 10:00:24 +0100

    implement volumes and extraSecrets and update versions

--- vars.sh
+++ vars.sh
@@ -1,16 +1,16 @@
 CPSYS=grpl-system
 PACKAGE=grsf.xpkg
 CONFIGPKG=grpl/grsf
-VERSION=0.2.0
+VERSION=0.2.1
 TESTNS=grpl-test
 
 PROVIDERKUBERNETES=false
 PROVIDERHELM=true
 
 BEBASEIMAGE="grpl/loopback"
-BEBASEIMAGEVERSION="v0.112"
+BEBASEIMAGEVERSION="v0.121"
 UIBASEIMAGE="grpl/cloud20x-ui-modules"
-UIBASEIMAGEVERSION="0.1.38"
+UIBASEIMAGEVERSION="0.1.40"
 
 if [ "$(uname -s)" = "Darwin" ]; then 
     SED=gsed
