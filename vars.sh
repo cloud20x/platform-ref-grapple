76e29a5750b473934370a23b529164e34687a33e
grapple-solutions <info@grapple-solutions.com>
2024-09-02 17:26:02 +0200

    upgrade to version 0.2.6

--- vars.sh
+++ vars.sh
@@ -1,16 +1,16 @@
 CPSYS=grpl-system
 PACKAGE=grsf.xpkg
 CONFIGPKG=grpl/grsf
-VERSION=0.2.5
+VERSION=0.2.6
 TESTNS=grpl-test
 
 PROVIDERKUBERNETES=false
 PROVIDERHELM=true
 
 BEBASEIMAGE="grpl/grapi"
-BEBASEIMAGEVERSION="0.2.5"
+BEBASEIMAGEVERSION="0.2.6"
 UIBASEIMAGE="grpl/gruim"
-UIBASEIMAGEVERSION="0.2.5"
+UIBASEIMAGEVERSION="0.2.6"
 
 if [ "$(uname -s)" = "Darwin" ]; then 
     SED=gsed
